4a072a91b4d48e9d0bd7072dc419ea7b296b60fd
xiang jie <xiangjie@example.com>
2017-10-26 17:26:45 +0800

    create project for testing doxygen

---
+++ doc/html/search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['simple_20device_20apis_20list_2e',['Simple device APIs list.',['../group__Dev__API.html',1,'']]],
+  ['simple_20device_20status_20information_2e',['Simple device status information.',['../group__Dev__Status.html',1,'']]]
+];
